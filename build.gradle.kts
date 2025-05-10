5edf76798801fbfe5ec6607802e48042589de034
Dmitriy Goncharov <incoom@yandex.ru>
2025-05-11 02:53:54 +0300

    CHORE: initialized the project

---
+++ build.gradle.kts
@@ -0,0 +1,54 @@
+plugins {
+    java
+    id("org.springframework.boot") version "3.4.5"
+    id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "hhz.ktoeto"
+version = "1.0.0"
+java.sourceCompatibility = JavaVersion.VERSION_21
+
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    val lombokVersion = "1.18.38"
+    val liquibaseVersion = "4.31.1"
+    val h2Version = "2.3.232"
+    val jacksonDatatypeVersion = "2.19.0"
+    val mapstructVersion = "1.6.3"
+
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:$jacksonDatatypeVersion")
+    implementation("org.liquibase:liquibase-core:$liquibaseVersion")
+    implementation("com.h2database:h2:$h2Version")
+    implementation("org.projectlombok:lombok:$lombokVersion")
+    implementation("org.mapstruct:mapstruct:$mapstructVersion")
+
+    annotationProcessor("org.projectlombok:lombok:$lombokVersion")
+    annotationProcessor("org.mapstruct:mapstruct-processor:$mapstructVersion")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.bootJar {
+    archiveFileName.set("transaction-service.jar")
+    destinationDirectory.set(file("./build/"))
+}
+
+tasks.compileJava {
+    options.encoding = "UTF-8"
+}
---
+++ src/main/java/hhz/ktoeto/transactionservice/Launcher.java
@@ -0,0 +1,12 @@
+package hhz.ktoeto.transactionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Launcher {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Launcher.class, args);
+    }
+}
